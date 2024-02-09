04c4d88ce7c769f5f0d2446cf1ab9d80f0255301
Robin Pahlman <robin.pahlman@gmail.com>
2024-02-09 12:00:45 -0500

    bio

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -5,17 +5,18 @@ import Textarea from 'primevue/textarea';
 const form = reactive({
   firstName: '',
   lastName: '',
+  email: '',
   message: ''
 })
 </script>
 <template>
-  <div class="form flex flex-col gap-1 my-4 justify-center items-center">
+  <div class="form flex flex-col gap-1 my-4 justify-center items-center bg-white py-8 my-8 rounded w-1/3 m-auto">
     <label for="firstname" class="text-black">First name</label>
     <InputText type="text" id="firstname" v-model="form.firstName" />
     <label for="lastname" class="text-black">Last name</label>
     <InputText type="text" id="lastname" v-model="form.lastName" />
     <label for="email" class="text-black">Email</label>
-    <InputText type="text" id="email" v-model="form.lastName" />
+    <InputText type="text" id="email" v-model="form.email" />
     <label for="message" class="text-black">Message</label>
     <Textarea id="message" v-model="form.message" rows="5" cols="30" />
   </div>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,11 @@
+<script setup>
+import rpImage from '@/assets/rp-guit2.png'
+</script>
 <template>
-  <div class="about bg-darkred">
-    <h1>This is an about page</h1>
+  <div class="about bg-darkred left-1/2 w-100">
+    <h1>Bio</h1>
+    <p>Robin Pahlman is a guitarist and prducer based in New York City. Originally from Finland, he received his music education in Stockholm, Sweden,and worked as a freelance musician and guitar teacher for 20+ years. He has worked with bands and played shows all over the world – from small DIY spaces in Vienna, Austria to Carnegie Hall in New York City.<br/><br/>Fluent in many genres and generally interested in everything, Robin has played punk rock, folk and country, rock, pop: electric, acoustic, and electronic. He released lo-fi folk music as part of the duo Carmen & The Devil, self-produced and recorded a folk-inspired solo EP, and produced his debut album in 2016. He performed and recorded with Deni Bonet and Howth in New York City. He is currrently a member of the band Automaatio and regularly performs with Grace Bergere.</p>
+    <img :src="rpImage" class="opacity-10">
   </div>
 </template>
 
