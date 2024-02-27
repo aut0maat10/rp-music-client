6d49026d2f1dd3628a81f675ec72e42705958dc4
Robin Pahlman <robin.pahlman@gmail.com>
2024-02-27 15:07:12 -0500

    font size contact view

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -5,7 +5,7 @@ import rpImage from '@/assets/rp-guit2.png'
   <div class="contact bg-darkred flex flex-col justify-center p-8">
     <img :src="rpImage" class="opacity-10 z-0 max-lg:hidden absolute">
     <h2 class="text-2xl lg:text-4xl text-magenta mb-2 lg:mb-4">Contact</h2>
-    <p class="text-xl lg:text-2xl">
+    <p class="text-xl">
       Need professional <span class="text-green">guitar tracks</span> for your project?
       <br/><br/>
       Looking for a <span class="text-green">versatile guitarist</span> for your next show?
@@ -16,9 +16,9 @@ import rpImage from '@/assets/rp-guit2.png'
       <br/><br/>
     </p>
     <div class="text-left z-20 lg:mt-8">
-      <p class="text-xl lg:text-2xl">Please send me a message through the</p>
-      <a class="text-green hover:text-magenta hover:bg-darkred hover:scale-110 z-100 text-xl lg:text-3xl" href="#contactform">contact form</a>
-      <p class="text-xl lg:text-2xl">at the bottom of the page.</p>
+      <p class="text-xl">Please send me a message through the</p>
+      <a class="text-green hover:text-magenta hover:bg-darkred hover:scale-110 z-100 text-xl" href="#contactform">contact form</a>
+      <p class="text-xl">at the bottom of the page.</p>
       <span class="pi pi-arrow-circle-down text-3xl mt-4"></span>
     </div>
     
