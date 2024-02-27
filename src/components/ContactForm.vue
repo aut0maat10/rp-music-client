f23b5651bd21425b52e62d8a5aaeb5bf93d72387
Robin Pahlman <robin.pahlman@gmail.com>
2024-02-27 10:33:38 -0500

    form submit with aws

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,14 +1,10 @@
 <script setup>
-// import { reactive } from 'vue'
-// import InputText from 'primevue/inputtext';
-import Button from 'primevue/button'
-// import Textarea from 'primevue/textarea';
 import axios from 'axios'
-
 import { useForm } from 'vee-validate';
 import * as yup from 'yup';
 
-const { errors, handleSubmit, defineField } = useForm({
+// vee-validate + yup
+const { errors, handleSubmit, defineField, meta } = useForm({
   validationSchema: yup.object({
     firstName: yup.string().required('Name is required'),
     lastName: yup.string(),
@@ -24,35 +20,16 @@ const [message, messageAttrs] = defineField('message');
 
 const onSubmit = handleSubmit(values => {
   alert(JSON.stringify(values, null, 2));
+  try {
+    const payload = JSON.stringify(values)
+    const response = axios.post(import.meta.env.VITE_FORM_API_URL, payload, {headers: {'Content-Type': 'application/json'}})
+    console.log(response)
+  } catch(error) {
+    console.log(error)
+  }  
 });
-
-// const schema = yup.object({
-//   email: yup.string().required().email(),
-// });
-
-// const form = reactive({
-//   firstName: '',
-//   lastName: '',
-//   email: '',
-//   message: ''
-// })
-// const handleSubmit = async () => {
-//   const payload = JSON.stringify({
-//     firstName: form.firstName,
-//     lastName: form.lastName,
-//     email: form.email,
-//     message: form.message
-//   })
-//   try {
-//     console.log('success')
-//     // const response = await axios.post(import.meta.env.VITE_FORM_API_URL, payload, {headers: {'Content-Type': 'application/json'}})
-//     // console.log(response)
-//   } catch(error) {
-//     console.log(error)
-//   }
-  
-// }
 </script>
+
 <template>
   <div id="contactform" class="flex flex-col justify-center bg-tapes bg-contain py-20">
     <h2 class="text-magenta font-bold text-5xl mx-auto mb-4">Contact</h2>
@@ -65,7 +42,7 @@ const onSubmit = handleSubmit(values => {
       <input type="text" id="email" class="border py-2 px-3 text-black w-full focus:outline focus:outline-green rounded" v-model="email" :emailAttrs="emailAttrs" />
       <label for="message" class="text-white text-lg self-start">Message *<span class="text-green ml-4">{{ errors.message }}</span></label>
       <textarea id="message" v-model="message" :messageAttrs="messageAttrs" rows="5" cols="30" class="border py-2 px-3 text-black w-full focus:outline focus:outline-green rounded" />
-      <Button class="bg-magenta px-8 py-2 mt-8 rounded hover:bg-green hover:text-magenta"><span class="pi pi-send mr-4 ml-0"></span>Send</Button>
+      <button :disabled="!meta.valid" class="bg-magenta px-8 py-2 mt-8 rounded hover:bg-green hover:text-magenta disabled:bg-darkred disabled:pointer-events-none"><span class="pi pi-send mr-4 ml-0"></span>Send</button>
     </form>
   </div>
 </template>
