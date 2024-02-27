d4cace2cd459c092bf74c5d8ea79af1538bc9f4e
Robin Pahlman <robin.pahlman@gmail.com>
2024-02-27 14:27:10 -0500

    edits to header

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@
       leave-to-class="transform opacity-0 scale-75"
       appear
     >
-      <h1 class="text-6xl text-magenta py-3 font-bold">Robin Pahlman</h1>
+      <h1 class="text-4xl lg:text-6xl text-magenta py-3 font-bold">Robin Pahlman</h1>
     </Transition>
     <Transition
       enter-active-class="delay-500 duration-500 ease-out"
@@ -23,7 +23,7 @@
       leave-to-class="transform opacity-0 scale-75"
       appear
     >
-    <h3 class="text-4xl pb-3 font-normal text-white">
+    <h3 class="text-2xl lg:text-4xl font-normal text-white">
       Musician <span class="invisible sm:visible">|</span> Songwriter <span class="invisible sm:visible">|</span> Producer
     </h3>
   </Transition>
