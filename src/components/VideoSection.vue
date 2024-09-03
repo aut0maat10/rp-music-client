8db273ea6b39d4255ac3e8a961a32dd629ebb9e4
Robin Pahlman <robin.pahlman@gmail.com>
2024-09-02 20:39:47 -0400

    update video content

--- src/components/VideoSection.vue
+++ src/components/VideoSection.vue
@@ -5,6 +5,9 @@
   <div class="w-full h-100 bg-white p-24 flex flex-col items-center">
   <!-- <h2 class="text-7xl text-magenta mb-12"><i class="pi pi-video"></i></h2> -->
     <div class="grid lg:grid-cols-2 gap-8 items-center justify-center">
+      <div>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/WkgBt1xShls?si=VYjSxezVlfNBSLdx&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
       <div class="">
         <iframe class="w-80 h-44 lg:w-[560px] lg:h-[315px]" src="https://www.youtube.com/embed/Vo4UzZhftVY?si=ObMvZkRNVuNOefpW&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
@@ -13,9 +16,6 @@
       </div>
       <div>
         <iframe class="w-80 h-44 lg:w-[560px] lg:h-[315px]" src="https://www.youtube.com/embed/uHOT9pn2tTw?si=u_Jdk1gOHTH0VCbV&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-      </div>
-      <div>
-        <iframe class="w-80 h-44 lg:w-[560px] lg:h-[315px]" src="https://www.youtube.com/embed/a3P6DXoDFyQ?si=Tdsu-WZPsRCnYJlo&amp;controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
     </div>
   </div>
